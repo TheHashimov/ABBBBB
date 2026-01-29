60385a0a00f22489a1a65a3de3106b91f5cf4178
TheHashimov <Thehashimov@gmail.com>
2026-01-30 02:58:12 +0400

    Initial project upload

---
+++ java/com/abbtech/AbbtechModuleThreeApplication.java
@@ -0,0 +1,13 @@
+package com.abbtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbbtechModuleThreeApplication {
+
+   public static void main(String[] args) {
+        SpringApplication.run(AbbtechModuleThreeApplication.class, args);
+    }
+
+}
